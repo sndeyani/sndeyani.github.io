53418e796f079f3d798dce2308c56be3f41ba92c
anisndeyan <ani@mergedup.com>
2022-09-20 23:04:02 +0400

    Downlaod cv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
 function App() {
-  return (
-    <div>
-      Hello Ani 
-    </div>
-  );
+
+	const download = () => {
+		fetch('Ani_Sndeyan_CV.pdf').then(response => {
+			response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Ani_Sndeyan_CV.pdf';
+                alink.click();
+            })
+		})
+	}
+
+	return (
+		<div>
+			
+			<button onClick={download}>Download CV</button>	
+		</div>
+	);
 }
 
 export default App;
